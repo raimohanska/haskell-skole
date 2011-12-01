244ff73293427bb857dfbeb8c7a8471690e559e9
Juha Paananen <juha.paananen@reaktor.fi>
2011-12-01 15:08:49 +0200

    add grep

--- Slide.hs
+++ Slide.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module Slide where
 
 import System.IO.Unsafe
@@ -5,6 +6,7 @@ import Data.IORef
 import Control.Monad
 import Data.List
 import Data.Maybe
+import System.Directory
 
 index = unsafePerformIO $ newIORef 0
 
@@ -21,3 +23,17 @@ prev  = relative (\i -> i - 1)
 go n  = relative $ const $ fromJust $ elemIndex n chars
 
 relative f = modifyIORef index f >> current
+grep s = findAll >>= mapM grep' >>= (mapM_ (putStrLn . format)) . concat
+  where grep' :: String -> IO [(FilePath, String, Int)] 
+        grep' f = readFile f >>= 
+          return . map (\(n, l) -> (f,l,n)) 
+                 . filter ((s `isInfixOf`) . snd) 
+                 . zip [1..] . lines
+        format :: (FilePath, String, Int) -> String
+        format (f, ln, num) = f ++ "(" ++ (show num) ++"): " ++ ln
+        findAll = forM fileNames maybeFileName >>= return . catMaybes 
+        maybeFileName f = doesFileExist f >>= return . ifTrue f 
+        fileNames = map (: ".lhs") chars
+        ifTrue x True  = Just x
+        ifTrue _ False = Nothing
+
