fec0a2fb5607d504614b4eaadb0a15cd6fed335b
Juha Paananen <juha.paananen@reaktor.fi>
2011-12-01 15:15:27 +0200

    cleanup

--- Slide.hs
+++ Slide.hs
@@ -23,9 +23,9 @@ prev  = relative (\i -> i - 1)
 go n  = relative $ const $ fromJust $ elemIndex n chars
 
 relative f = modifyIORef index f >> current
-grep s = findAll >>= mapM grep' >>= (mapM_ (putStrLn . format)) . concat
-  where grep' :: String -> IO [(FilePath, String, Int)] 
-        grep' f = readFile f >>= 
+grep s = findAll >>= mapM grepInFile >>= (mapM_ (putStrLn . format)) . concat
+  where 
+        grepInFile f = readFile f >>= 
           return . map (\(n, l) -> (f,l,n)) 
                  . filter ((s `isInfixOf`) . snd) 
                  . zip [1..] . lines
